3fe648144d5e98ec1a00f57e056c3e507cf70255
Steven Vi <visteven00@gmail.com>
2022-07-23 17:32:47 +1000

    attempts

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,26 @@
-type MyPick<T, K> = any
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+// type MyPick<T, K extends keyof T> = {
+//   K: T[K]
+// }
+
+type MyPick<TObj, TKey extends keyof TObj> = {
+  [SpecificKey in TKey]: TObj[SpecificKey];
+}
+
+type Yeah = MyPick<Todo, 'title'>
+
+type Test<TObj, TKey extends keyof TObj> = {
+  [Key in TKey]: TObj[TKey]
+}
+
+type Test2 = Test<{ a: string }, 'a'>
+
+const data: Test2 = {
+  a: 'test',
+}
+
